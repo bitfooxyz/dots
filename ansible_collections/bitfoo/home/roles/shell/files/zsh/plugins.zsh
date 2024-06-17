e9e2a33a55522910496de1a0520b7b4096e93001
Daniel von Eßen <daniel@vonessen.eu>
2024-06-17 07:49:56 +0200

    feat(shell): :sparkles: Added zoxide eval command to bash and zsh.

--- ansible_collections/bitfoo/home/roles/shell/files/zsh/plugins.zsh
+++ ansible_collections/bitfoo/home/roles/shell/files/zsh/plugins.zsh
@@ -56,3 +56,7 @@ if command -v aws_completer &> /dev/null; then
   autoload bashcompinit && bashcompinit
   complete -C 'aws_completer' aws
 fi
+
+if command -v zoxide &> /dev/null; then
+  eval $(zoxide init zsh --cmd cd)
+fi
