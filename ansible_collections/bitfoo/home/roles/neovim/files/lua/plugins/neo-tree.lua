f5b164e21f701e7e7f75edf63d649ecfb65e6d74
Daniel von Eßen <daniel@vonessen.eu>
2024-09-12 14:09:19 +0200

    feat(nvim): Added configuration for neo-tree

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/neo-tree.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/neo-tree.lua
@@ -7,8 +7,37 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
+    opts = {
+      sort_case_insensitive = true,
+      popup_border_style = "rounded",
+      use_popups_for_input = true,
+      source_selector = {
+        winbar = true, -- toggle to show selector on winbar
+      },
+      default_component_configs = {
+        indent = {
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+          indent_marker = "│",
+          indent_size = 2,
+          last_indent_marker = "└",
+          with_expanders = true,
+          with_markers = true,
+        },
+        name = {
+          trailing_slash = true,
+          highlight_opened_files = true,
+        },
+        git_status = {
+          symbols = { modified = "Δ" },
+        },
+      },
+    },
     init = function()
-      vim.keymap.set("n", "<leader>te", "<cmd>Neotree<CR>", { desc = "Toggle NeoTree file [E]xplorer" })
+      vim.keymap.set("n", "<leader>te", function()
+        require("neo-tree.command").execute({ toggle = true, action = "focus", source = "filesystem", reveal = true })
+      end, { desc = "Toggle NeoTree file [E]xplorer" })
     end,
   },
 }
