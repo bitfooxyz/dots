10490069b15de6463c706080b35934c857d7bddb
Daniel von Eßen <daniel@vonessen.eu>
2024-09-19 08:51:30 +0200

    refactor: Refactored toggleterm.lua

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/toggleterm.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/toggleterm.lua
@@ -1,7 +1,6 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    version = "*",
     opts = {
       size = function(term)
         if term.direction == "horizontal" then
@@ -12,15 +11,9 @@ return {
       end,
     },
     init = function()
-      -- Create toggleterm keymap with better control
-      vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
-      --
-      -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
-      -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
-      -- is not what someone will guess without a bit more experience.
-      -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
-      -- or just use <C-\><C-n> to exit terminal mode
-      vim.keymap.set("t", "<Esc><Esc>", "<cmd>ToggleTerm<CR>", { desc = "Exit terminal mode" })
+      -- Toggle terminal
+      vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "[t]oggle [t]erminal" })
+
       --  See `:help window-resize` for a list of all window commands
       vim.keymap.set("t", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
       vim.keymap.set("t", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -32,6 +25,7 @@ return {
       vim.keymap.set("t", "<M-l>", "3<C-w>>", { silent = false, desc = "Move focus to the right window" })
       vim.keymap.set("t", "<M-k>", "3<C-w>+", { silent = false, desc = "Move focus to the lower window" })
       vim.keymap.set("t", "<M-j>", "3<C-w>-", { silent = false, desc = "Move focus to the upper window" })
+
       -- Require Terminal class from toggleterm
       local Terminal = require("toggleterm.terminal").Terminal
 
