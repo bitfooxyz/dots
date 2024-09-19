ac79743902731763d9e924bc7941e4b3c1cb079c
Daniel von Eßen <daniel@vonessen.eu>
2024-09-19 06:41:28 +0200

    lint: Removed comments

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/comments.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/comments.lua
@@ -1,9 +1,5 @@
 return {
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-
   -- "gc" to comment visual regions/lines
+  -- "gcc" to toggle comment
   { "numToStr/Comment.nvim", opts = {} },
 }
--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/conform.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/conform.lua
@@ -28,7 +28,6 @@ return {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         python = { "ruff_organize_imports", "ruff_fix", "ruff_format" },
-        --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
