5d3919976ce3171218724286b4432626f34668ec
Daniel von Eßen <daniel@vonessen.eu>
2024-09-19 06:42:55 +0200

    feat: :sparkles: Added buffer as completion source

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/nvim-cmp.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/nvim-cmp.lua
@@ -100,6 +100,7 @@ return {
         sources = {
           { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" },
+          { name = "buffer" },
           { name = "luasnip" },
           { name = "path" },
         },
