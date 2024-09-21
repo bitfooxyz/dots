26473cd51464d6511c313b22fa60bfc96ef26d23
Daniel von Eßen <daniel@vonessen.eu>
2024-09-21 09:42:35 +0200

    fix(wezterm): :bug: Fixed missing check if variable in secrets.lua

--- ansible_collections/bitfoo/home/roles/wezterm/files/hyperlink_rules.lua
+++ ansible_collections/bitfoo/home/roles/wezterm/files/hyperlink_rules.lua
@@ -4,14 +4,16 @@ local secrets = require("secrets")
 local M = {}
 
 function M.setup(config)
-	-- Use the defaults as a base
-	config.hyperlink_rules = wezterm.default_hyperlink_rules()
+  -- Use the defaults as a base
+  config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
-	-- Regex represents typical Jira Ticket Numbers like TEST-1234
-	table.insert(config.hyperlink_rules, {
-		regex = [[\b([A-Z][A-Z0-9_]+-[1-9][0-9]*)]],
-		format = secrets.jira_url .. "$1",
-	})
+  -- Regex represents typical Jira Ticket Numbers like TEST-1234
+  if secrets.jira_url then
+    table.insert(config.hyperlink_rules, {
+      regex = [[\b([A-Z][A-Z0-9_]+-[1-9][0-9]*)]],
+      format = secrets.jira_url .. "$1",
+    })
+  end
 end
 
 return M
