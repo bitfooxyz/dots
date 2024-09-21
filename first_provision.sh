b395108af77950a0382ed13a279956a103e0a7ee
Daniel von Eßen <daniel@vonessen.eu>
2024-09-21 09:37:28 +0200

    feat: :sparkles: Added better first_provision.sh experience

--- first_provision.sh
+++ first_provision.sh
@@ -2,9 +2,26 @@
 
 set -eo pipefail
 
+# Exporting local binary path
+export PATH="$HOME/.local/bin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
+
 if [[ -n "$DEBUG" ]]; then
    echo "Enable debug output"
    set -x
+fi
+
+# Prerequisites for macos
+if [[ $(uname) == "Darwin" ]]; then
+  # check if xcode is installed on Darwin systems
+  if [[ ! -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
+    xcode-select --install
+    echo -n "xcode installed? Press [y]: "
+    read XCODE
+    if [[ ! "$XCODE" == "y" ]]; then
+      echo "xcode not installed. User interruped."
+      exit 1
+    fi
+  fi
 fi
 
 if [[ ! -d .venv ]]; then
@@ -14,7 +31,7 @@ fi
 
 echo "Source venv and install dependencies from requirements.lock"
 source .venv/bin/activate
-pip3 install -r requirements.lock
+pip3 install ansible
 
 mkdir -p ./secrets/
 if [[ ! -f secrets/become_password_file ]]; then
@@ -34,5 +51,8 @@ fi
 echo "Install ansible galaxy requirements from requirements.yml"
 ansible-galaxy install -r requirements.yml
 
+echo -n "Enter play name [without .yml]: "
+read PLAYBOOK
+
 echo "Running ansible-playbook"
-ansible-playbook main.yml --diff --become-password-file ./secrets/become_password_file --vault-password-file ./secrets/vault_password_file
+ansible-playbook plays/${PLAYBOOK}.yml --diff --become-password-file ./secrets/become_password_file --vault-password-file ./secrets/vault_password_file $@
