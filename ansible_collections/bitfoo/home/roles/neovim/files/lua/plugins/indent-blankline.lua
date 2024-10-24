4c9175f78cca27f3d635c4654466d075bb84bde9
Daniel von Eßen <daniel@vonessen.eu>
2024-10-24 13:05:04 +0200

    feat(nvim): Create global disabled_filetypes list to be used to disable feature on certain types

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/indent-blankline.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/indent-blankline.lua
@@ -2,6 +2,8 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = { exclude = { filetypes = { "dashboard" } } },
+    ---@module 'ibl'
+    ---@type ibl.config
+    opts = { exclude = { filetypes = vim.g.disabled_filetypes } },
   },
 }
