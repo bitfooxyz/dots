362ad6880c20ea7496ee3450c36a149c74b7a873
Daniel von Eßen <daniel@vonessen.eu>
2024-10-24 20:04:35 +0200

    feat(nvim): :sparkles: Added virt-columns plugin to nvim

---
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/virt-column.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "lukas-reineke/virt-column.nvim",
+    opts = {
+      virtcolumn = "80,100,120,140",
+      exclude = {
+        filetypes = {
+          vim.g.disabled_filetypes,
+        },
+      },
+    },
+  },
+}
