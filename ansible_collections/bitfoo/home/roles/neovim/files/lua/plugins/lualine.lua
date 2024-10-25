f7ef9949eb2b0a26cd803a07d06f3c706b540288
Daniel von Eßen <daniel@vonessen.eu>
2024-10-25 12:35:57 +0200

    feat(nvim): :sparkles: Added lualine as a replacement for mini.statusline

---
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/lualine.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = { disabled_filetypes = { statusline = vim.g.disabled_filetypes, winbar = vim.g.disabled_filetypes } },
+    },
+  },
+}
