cab4810cc95ad4a5f0398a032c3f2386a21362c8
Daniel von Eßen <daniel@vonessen.eu>
2024-10-27 13:17:55 +0100

    feat(nvim): :sparkles: Added move line movin plugin mini.move

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/mini.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/mini.lua
@@ -18,6 +18,21 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require("mini.surround").setup()
       require("mini.comment").setup({})
+      require("mini.move").setup({
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<C-h>",
+          right = "<C-l>",
+          down = "<C-j>",
+          up = "<C-k>",
+
+          -- Move current line in Normal mode
+          line_left = "<C-h>",
+          line_right = "<C-l>",
+          line_down = "<C-j>",
+          line_up = "<C-k>",
+        },
+      })
     end,
   },
 }
