b2ac3cab41b28bef7f9348c590f643a5d787551a
Daniel von Eßen <daniel@vonessen.eu>
2024-10-29 06:23:59 +0100

    feat(nvim): :sparkles: Changed mappings for mini.move

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/mini.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/mini.lua
@@ -21,16 +21,16 @@ return {
       require("mini.move").setup({
         mappings = {
           -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-          left = "<C-h>",
-          right = "<C-l>",
-          down = "<C-j>",
-          up = "<C-k>",
+          left = "<M-h>",
+          right = "<M-l>",
+          down = "<M-j>",
+          up = "<M-k>",
 
           -- Move current line in Normal mode
-          line_left = "<C-h>",
-          line_right = "<C-l>",
-          line_down = "<C-j>",
-          line_up = "<C-k>",
+          line_left = "<M-h>",
+          line_right = "<M-l>",
+          line_down = "<M-j>",
+          line_up = "<M-k>",
         },
       })
     end,
