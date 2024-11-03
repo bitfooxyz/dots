e92b4390f4120ebf3f3707f146d3212c70027063
Daniel von Eßen <daniel@vonessen.eu>
2024-11-03 09:18:51 +0100

    feat(nvim): :sparkles: Added tokyonight theme to lualine

--- ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/lualine.lua
+++ ansible_collections/bitfoo/home/roles/neovim/files/lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = { disabled_filetypes = { statusline = vim.g.disabled_filetypes, winbar = vim.g.disabled_filetypes } },
+      theme = "tokyonight",
     },
   },
 }
